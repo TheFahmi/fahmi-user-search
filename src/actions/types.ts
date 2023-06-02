0ae4bf6a7ffd0efc28a3c4c9ec897aaa78d260e8
M Fahmi Hassan <fahmi@rumahsiapkerja.com>
2023-06-02 09:35:43 +0700

    fix data type any

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -3,12 +3,25 @@ export const GET_USER = 'GET_USER';
 
 export interface SearchUsersAction {
   type: typeof SEARCH_USERS;
-  payload: any[];
+  payload: User[];
 }
 
 export interface GetUserAction {
   type: typeof GET_USER;
-  payload: any;
+  payload: UserDetails;
 }
 
 export type UserActionTypes = SearchUsersAction | GetUserAction;
+
+interface User {
+  id: number;
+  login: string;
+}
+
+interface UserDetails {
+  id: number;
+  name: string;
+  description: string;
+  stargazers_count: number;
+}
+
